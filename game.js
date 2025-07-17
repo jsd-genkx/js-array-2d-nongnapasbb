a816b0a9302bb831a25db377a39b14d45cdc64e1
nongnapasbb <nongnapasbb@gmail.com>
2025-07-17 10:44:03 +0700

    game solution

--- game.js
+++ game.js
@@ -1,26 +1,25 @@
 "use strict";
 const board = [
-	["A", "B", "C"],
-	["D", "E", "F"],
-	["G", "H", "I"],
+  ["A", "B", "C"],
+  ["D", "E", "F"],
+  ["G", "H", "I"],
 ];
-
 let x = 0;
 let y = 0; // Start at A (0,0) -> [row][col]
-
 const moves = [];
 moves.push(board[x][y]); // A
-
 // Move Right to B
 y++;
 moves.push(board[x][y]); // B
-
 // Move Right to C
-
+y++;
+moves.push(board[x][y]);
 // Move Down to F
-
+x++;
+moves.push(board[x][y]);
 // Move left to E
-
+y--;
+moves.push(board[x][y]);
 console.log("Path:", moves.join(" → "));
 // Output: Path: A → B → C → F → E
 console.log("Total Moves (Start from 'A'):", moves.length - 1);
